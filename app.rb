ff8afdc2d5747e8d511cee1b3e5a6640fb9f0e69
Shea Kusiak <shkusiak@gmail.com>
2018-07-21 18:56:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ class App < Sinatra::Base
 
   post '/student' do
     @student = Student.new(params[:student])
-   
+
     params[:student][:courses].each do |details|
       Course.new(details)
     end
-   
+
     @courses = Course.all
-   
+
     erb :student
   end
-  
+
 end
